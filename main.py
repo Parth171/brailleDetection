54c2e0341664b04122782781c0340e21a89999d6
Parth Sakpal <p.sakpal@share.espb.ca>
2023-09-11 17:25:06 -0600

    Commit Test p3

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for train
 
 model.train(data='C:/Users/User/PycharmProjects/brailleDetection/data/braille_data', epochs=60, imgsz=64)
 
-## This is a test so see if the commits are working - part 2
+## This is a test so see if the commits are working - part 3
